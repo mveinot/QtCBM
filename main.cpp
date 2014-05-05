32a385a7fcd5b42ff4e1a88ea0eb75e10a483f56
Mark Veinot <mveinot@gmail.com>
2014-05-05 09:49:54 -0300

    attempt to cause a running instance to notify in some way when a second instance is started

--- main.cpp
+++ main.cpp
@@ -5,16 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-    QtSingleApplication a(argc, argv);
+    QtSingleApplication a("QtCBM", argc, argv);
 
     if (a.isRunning())
     {
+        a.sendMessage("Already started");
         QMessageBox::warning(NULL, "QtCBM", "Another instance of this application is already running. Press OK to exit.", QMessageBox::Ok, QMessageBox::Ok);
         return 1;
     }
 
     FileWindow w;
     w.show();
+
+    a.setActivationWindow(&w);
+
     if (argc > 1)
         w.writeD64FromArgs(QString(argv[1]));
 
