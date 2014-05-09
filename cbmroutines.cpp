26a60190720d795f45a68c82f121925c6c2003cd
Mark Veinot <mveinot@gmail.com>
2014-05-09 19:42:34 -0300

    Incorporate diskimage library

---
+++ cbmroutines.cpp
@@ -0,0 +1,5 @@
+#include "cbmroutines.h"
+
+CBMroutines::CBMroutines()
+{
+}
---
+++ cbmroutines.h
@@ -0,0 +1,10 @@
+#ifndef CBMROUTINES_H
+#define CBMROUTINES_H
+
+class CBMroutines
+{
+public:
+    CBMroutines();
+};
+
+#endif // CBMROUTINES_H
