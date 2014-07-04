def0567c03540f91d1c07e209c1d1ca1e9db03c5
Mark Veinot <mveinot@gmail.com>
2014-07-04 08:52:15 -0300

    Custom debug message handling

--- main.cpp
+++ main.cpp
@@ -1,10 +1,55 @@
 #include "filewindow.h"
 #include <QApplication>
+#include <QtDebug>
 #include <QtSingleApplication>
 #include <QMessageBox>
+#include <QStandardPaths>
+
+void localMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QString txt;
+
+    switch (type)
+    {
+        case QtDebugMsg:
+            txt = QString("DBUG: %1").arg(msg);
+            break;
+        case QtWarningMsg:
+            txt = QString("WARN: %1 in %2 at line %3").arg(msg).arg(context.file).arg(context.line);
+            break;
+        case QtCriticalMsg:
+            txt = QString("CRIT: %1 in %2 at line %3").arg(msg).arg(context.file).arg(context.line);
+            break;
+        case QtFatalMsg:
+            txt = QString("FATL: %1 in %2 at line %3").arg(msg).arg(context.file).arg(context.line);
+            break;
+        default:
+            txt = QString("Undefined message type. Message: %1").arg(msg);
+    }
+
+    QString directory = QStandardPaths::locate(QStandardPaths::DataLocation, QString(), QStandardPaths::LocateDirectory);
+    if (directory.isEmpty())
+        directory = QDir::homePath() + "/." + QCoreApplication::applicationName();
+
+    //QMessageBox::information(0, "directory", directory, QMessageBox::Ok, QMessageBox::Ok);
+
+    if (!QFile::exists(directory))
+    {
+        QDir dir;
+        dir.mkpath(directory);
+    }
+
+    QFile outFile(directory + "/messages.log");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+    ts.flush();
+    outFile.close();
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(localMessageHandler);
     QtSingleApplication a("QtCBM", argc, argv);
 
     if (a.isRunning())
